9c9b02e5d27a44cae0f431f94e5463b7af7a5adc
Samb1232 <arterm212@gmail.com>
2022-09-19 02:16:43 +0300

    Task2

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function getRandomNumber(min, max) {
+/* Returns a random number from min to max, both included.*/
+  if (min > max) throw new RangeError('Wrong range. Argument min should be lower then max');
+
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+function ifStringShort(string, size) {
+/* Returns true if string is shorter then size, and false otherwise. */
+  return String(string).length <= size;
+}
