804b871532abc9005f290ba579e39e37951c8954
Keks <keks@htmlacademy.ru>
2022-12-20 00:51:25 +0300

    :heavy_check_mark: Сборка #11

---
+++ 11/js/post_validaton.js
@@ -0,0 +1,54 @@
+const form = document.querySelector('.img-upload__form');
+const hashtagsField = form.querySelector('.text__hashtags');
+const submitButton = form.querySelector('.img-upload__submit');
+
+const description = form.querySelector('.text__description');
+
+const hashtagRegex = /(^#[A-Za-zА-Яа-яЁё0-9]{1,19}$)|(^\s*$)/
+
+
+
+
+let pristine = new Pristine(form, {
+    classTo: 'img-upload__field-wrapper',
+    errorClass: 'text-invalid',
+    successClass: 'text-valid',
+    errorTextParent: 'img-upload__field-wrapper',
+    errorTextTag: 'div',
+    errorTextClass: 'text-invalid__error'
+}, true);
+
+
+pristine.addValidator(
+    hashtagsField,
+    (value) => {
+        const hashtagList = value.split(' ');
+
+        let isHashtagRight = true;
+        hashtagList.forEach(text => {
+            isHashtagRight = isHashtagRight & hashtagRegex.test(text);
+        });
+        return isHashtagRight;
+    },
+    'incorrect hashtag'
+);
+
+pristine.addValidator(
+    hashtagsField,
+    (hashtag) => {
+        const keys = hashtag.split(' ').map((tag) => tag.toLowerCase());
+
+        return (new Set(keys).size == keys.length);
+    },
+    'Hashtags is not uniuque'
+);
+
+pristine.addValidator(
+    description,
+    (value) => value.length < 140,
+    'Comment is too big'
+);
+
+
+export {pristine};
+
---
+++ 11/js/util.js
@@ -0,0 +1,58 @@
+const getRandomPositiveInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+}
+
+const checkStringLength = (string, size) => {
+/* Returns true if string is shorter then size, and false otherwise. */
+  return String(string).length <= size;
+}
+
+const isEsc = (event) => event.key === 'Escape';
+
+
+// Источник - https://www.freecodecamp.org/news/javascript-debounce-example
+
+function debounce (callback, timeoutDelay = 500) {
+  let timeoutId;
+
+  return (...rest) => {
+    clearTimeout(timeoutId);
+
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+}
+
+// Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_throttle
+
+function throttle (callback, delayBetweenFrames) {
+  let lastTime = 0;
+
+  return (...rest) => {
+    const now = new Date();
+    if (now - lastTime >= delayBetweenFrames) {
+      callback.apply(this, rest);
+      lastTime = now;
+    }
+  };
+}
+
+
+function getRandomUniqueElements(arr, n) {
+  const result = new Array(n);
+  let len = arr.length;
+  const taken = new Array(len);
+  while (n--) {
+    let x = Math.floor(Math.random() * len);
+    result[n] = arr[x in taken ? taken[x] : x];
+    taken[x] = --len in taken ? taken[len] : len;
+  }
+  return result;
+};
+
+
+export {getRandomPositiveInteger, checkStringLength, isEsc, debounce, throttle, getRandomUniqueElements};
